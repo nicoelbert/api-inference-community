1088aa83fe243c44a596d59673409325e194e408
nicoelbert <30658685+nicoelbert@users.noreply.github.com>
2022-05-25 20:12:06 +0200

    adjust image_segmentation pipeline for multiple models and with correct output

--- docker_images/deepflash2/app/pipelines/image_segmentation.py
+++ docker_images/deepflash2/app/pipelines/image_segmentation.py
@@ -9,6 +9,11 @@ from deepflash2.all import *
 from pathlib import Path
 from PIL import Image
 
+from huggingface_hub import hf_hub_download
+import os
+import shutil
+
+
 
 #open tasks
 ## replace token
@@ -18,46 +23,50 @@ from PIL import Image
 ##adjust model name and second test scenario in test case
 ## try to delete other sample files
 
+#config file für modelle
+#repo und modelid
+
 
 ################## stuff that needs to be replaced/moved ###################
-API_TOKEN = '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'
+#API_TOKEN = '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'
 # deepflash2 config class
 
 
-def deepflash2_load_models(self,model_id: str, models_dir="./models"):
-    #to be replaced
-    repo_id="nicoelbert/deepflash2_testrepo"
-    model_filename = model_id + '.pth'
+def deepflash2_load_models(repo_id: str, models_dir="./models"):
 
-    #download pth file
-    from huggingface_hub import hf_hub_download
-    temp_dir = hf_hub_download(repo_id=repo_id, filename=model_filename)
+    #repo_id is passed as model_id
 
-    #create new dir
-    import os
+    #download ensemble_config with filenames
+    ensemble_json = hf_hub_download(repo_id=repo_id, filename="ensemble.json")
+    with open(ensemble_json, 'r') as f:
+        data = json.load(f)
+    models = data['models']
+
+    #create models directory
     os.makedirs(models_dir, exist_ok=True)
 
-    #copy pth file
-    import shutil
-    shutil.copy(temp_dir, f'{models_dir}/{model_filename}')
-
-
+    for model_fn in models:
+        # download pth file
+        temp_dir = hf_hub_download(repo_id=repo_id, filename=model_fn)
+        shutil.copy(temp_dir, f'{models_dir}/{model_fn}')
 
 
 ################## stuff that needs to be done againg ###################
 
 ##########################################################################
-class ImageSegementationPipeline(Pipeline):
+class ImageSegementationPipeline():
     def __init__(self, model_id: str, SEED = 0, models_dir = './models'):
         # IMPLEMENT_THIS
         # Preload all the elements you are going to need at inference.
         # For instance your model, processors, tokenizer that might be needed.
         # This function is only called once, so do all the heavy processing I/O here
+        self.repo_id = model_id
 
-        self.cfg = cfg = Config(random_state=SEED)
-        self.models_dir = self.models_dir
+        self.cfg = Config(random_state=SEED)
+        self.models_dir = models_dir
 
-        deepflash2_load_models(model_id, self.models_dir)
+        deepflash2_load_models(self.repo_id, models_dir = self.models_dir)
+
 
     def __call__(self, inputs: Image.Image) -> List[Dict[str, Any]]:
         """
@@ -70,36 +79,32 @@ class ImageSegementationPipeline(Pipeline):
                 It is preferred if the returned list is in decreasing `score` order
         """
         # save image into dir
-        import os
         os.makedirs('images', exist_ok=True)
-        inputs.save('images/this_img.tif')
+        inputs.save('images/this_img.png')
 
         # specify paths
         test_data_path = './'
-        ensemble_path = self.models_dir
-        prediction_path = Path('./')
 
         #create model
-        model = EnsembleLearner('images',
-                                path=test_data_path,
+        self.model = EnsembleLearner('images',
+                                path=Path(test_data_path),
                                 config=self.cfg,
-                                ensemble_path=ensemble_path)
+                                ensemble_path=Path(self.models_dir))
+
+        # specify paths
+        test_data_path = './'
+        prediction_path = Path('./')
 
         # Predict and save semantic segmentation masks
-        model.get_ensemble_results(model.files,
+        g_smx, g_std, g_eng = self.model.get_ensemble_results(self.model.files,
                                      use_tta=True,
                                      export_dir=prediction_path / 'masks')
 
-        # Save uncertainty scores
-        df_unc = model.df_ens[['file', 'ensemble', 'n_models', 'uncertainty_score']]
-        df_unc.to_csv(prediction_path / 'uncertainty_scores.csv', index=False)
+        self.model.show_ensemble_results()
 
-        # Show results scores
-        model.show_ensemble_results()
+        pred = np.argmax(g_smx['this_img.png'][:], axis=-1).astype('uint8')
 
-        #uncertainty scores
-        #return df_unc
-        test_return_list = ["test"]
-        return test_return_list
+        return pred
+
 
 
