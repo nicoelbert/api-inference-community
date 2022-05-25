d0b23ce83244f293d032869e4e8aa18e55cefaaf
nicoelbert <30658685+nicoelbert@users.noreply.github.com>
2022-05-25 22:15:42 +0200

    reverse changes made on timm library

--- docker_images/timm/app/pipelines/__init__.py
+++ docker_images/timm/app/pipelines/__init__.py
@@ -1,3 +1,3 @@
 from app.pipelines.base import Pipeline, PipelineException  # isort:skip
 
-from app.pipelines.image_segmentation import ImageSegmentationPipeline
+from app.pipelines.image_segmentation import ImageClassificationPipeline
